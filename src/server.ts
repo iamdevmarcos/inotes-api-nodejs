8c8b4ed927b0b50f796110be661595e9e97785f7
Marcos Andre <marcosdev.me@gmail.com>
2021-12-04 11:15:58 -0300

    creating base of the project

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, ErrorRequestHandler } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ error: 'endpoint not found...' });
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.log(err);
+    res.status(400).json({ error: 'error...' });
+}
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT);
